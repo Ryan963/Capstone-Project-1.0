168ae8d1487e43abdd99309b1f471cc94892163f
Ryan Curtis <curtisr7@mymacewan.ca>
2022-09-21 20:14:37 -0600

    Initialize files for user model and authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,3 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
---
+++ backend/models/userModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+})
