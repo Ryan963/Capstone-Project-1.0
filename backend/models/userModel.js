5851d4ca90cef31b0a1dbc4e5e2811c88824d5c6
Ryan Curtis <curtisr7@mymacewan.ca>
2022-09-22 20:28:22 -0600

    Create user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,58 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    name: {
+    firstname: {
         type: String,
-        required: [true, 'Please add a name']
+        required: [true, 'Please add a First Name']
     },
-})
+    lastname: {
+        type: String,
+        required: [true, 'Please add a Last Name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an Email'],
+        unique: true
+    },    
+    password: {
+        type: String,
+        required: [true, 'Please add a Password']
+    },
+    majors: {
+        type: Array,
+        required: false
+    },
+    minors: {
+        type: Array,
+        required: false
+    },
+    degree: {
+        type: String,
+        required: [true, 'Please add a Degree']
+    },
+    courses: {
+        type: Array,
+        required: false
+    },
+    currentyear: {
+        type: Number,
+        required: false
+    },
+    currentsemester: {
+        type: String,
+        required: false
+    },
+    graduated: {
+        type: Boolean,
+        required: false
+    },
+    gpa: {
+        type: Number,
+        required: false
+    }},
+    {
+        timeStamps: true
+    }   
+)
+
+module.exports = mongoose.model("User", userSchema)
