bd573ad79701564c671663bb68dedb1859f07ead
sawyerg3 <sawyerg333@gmail.com>
2022-11-26 23:48:35 -0700

    Add requirement filter to course page

--- backend/routes/recommendationsRoutes.js
+++ backend/routes/recommendationsRoutes.js
@@ -4,10 +4,11 @@ const { protect } = require("../middleware/userAuthMiddleware");
 const {
   recommendCourses,
   requirementsSatisfied,
+  getAllUserRequirements,
 } = require("../controllers/recommendationsController");
 
 router.route("/").get(protect, recommendCourses);
-
+router.route("/requirements").get(protect, getAllUserRequirements);
 router.route("/:id").get(protect, requirementsSatisfied);
 
 module.exports = router;
