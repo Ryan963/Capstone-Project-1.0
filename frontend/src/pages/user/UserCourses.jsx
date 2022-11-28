982cf1dc0ce72bd0bd51cc5844f4b6d4d5fc3eb5
sawyerg3 <sawyerg333@gmail.com>
2022-11-27 23:29:20 -0700

    Add ellipsis text overflow

--- frontend/src/pages/user/UserCourses.jsx
+++ frontend/src/pages/user/UserCourses.jsx
@@ -416,7 +416,7 @@ export default function UserCourses() {
                 </label>
 
                 <select
-                  className=" w-40 p-2.5 m-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className=" text-ellipsis w-40 p-2.5 m-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   name="filterByCompletion"
                   id="filterByCompletion"
                   value={filterByRequirement}
