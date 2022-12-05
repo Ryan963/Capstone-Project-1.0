b29b21ac4e89f9d0d3490f7bb0e6c8eeb4500d2c
sawyerg3 <sawyerg333@gmail.com>
2022-12-04 21:03:52 -0700

    Add filters to admin courses page

--- frontend/src/pages/admin/Courses.jsx
+++ frontend/src/pages/admin/Courses.jsx
@@ -18,6 +18,19 @@ const Courses = () => {
   const [showAddCourseModal, setShowAddCourseModal] = useState(false);
   const [showDeleteObjectModal, setShowDeleteObjectModal] = useState(false);
   const [showUpdateCourseModal, setShowUpdateCourseModal] = useState(false);
+  const [filterByDiscipline, setFilterByDiscipline] = useState("");
+  const [filterByLevel, setFilterByLevel] = useState("");
+
+  function getDisciplines(courses) {
+    const disciplines = [];
+    for (let course of courses) {
+      if (!disciplines.includes(course.discipline)) {
+        disciplines.push(course.discipline);
+      }
+    }
+    return disciplines.sort();
+  }
+  const disciplines = getDisciplines(courses);
 
   const addCourse = (course) => {
     const newCourseList = [...courses, course];
@@ -46,6 +59,46 @@ const Courses = () => {
       <div className="ml-20 w-full mr-20">
         {courses.length > 0 ? (
           <>
+            <div className="flex justify-between">
+              <div>
+                <label className="font-semibold text-lg">
+                  Course Discipline:
+                </label>
+
+                <select
+                  // className="w-40 m-2 border rounded-md border-primary "
+                  className=" w-40 p-2.5 m-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  id="grid-first-name"
+                  type="text"
+                  name="discipline"
+                  onChange={(e) => setFilterByDiscipline(e.target.value)}
+                  value={filterByDiscipline}
+                >
+                  <option value={""}>All</option>
+                  {disciplines.map((discipline, idx) => (
+                    <option key={idx} value={discipline}>
+                      {discipline}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <label className="font-semibold text-lg">Course Level:</label>
+                <select
+                  className=" w-40 p-2.5 m-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  name="filterByLevel"
+                  id="filterByLevel"
+                  value={filterByLevel}
+                  onChange={(e) => setFilterByLevel(e.target.value)}
+                >
+                  <option value={""}>All</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+            </div>
             <div
               className={`flex mt-1 p-3 items-center rounded-3xl border bg-lightgrey text-grey`}
             >
@@ -73,82 +126,99 @@ const Courses = () => {
               </div>
             </div>
             <div>
-              {courses.map((course, idx) => {
-                return (
-                  <div
-                    key={course._id}
-                    className={`flex mt-1 p-3 items-center rounded-3xl border  ${
-                      idx % 2 === 1 ? "bg-lightblue2" : ""
-                    }`}
-                  >
+              {courses
+                .filter((course) => {
+                  if (filterByDiscipline.length > 0) {
+                    if (course.discipline !== filterByDiscipline) {
+                      return false;
+                    }
+                  }
+                  if (filterByLevel.length > 0) {
+                    if (course.name[course.name.length - 3] !== filterByLevel) {
+                      return false;
+                    }
+                  }
+                  return true;
+                })
+                .map((course, idx) => {
+                  return (
                     <div
-                      style={{
-                        whiteSpace: "nowrap",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        width: "240px",
-                      }}
+                      key={course._id}
+                      className={`flex mt-1 p-3 items-center rounded-3xl border  ${
+                        idx % 2 === 1 ? "bg-lightblue2" : ""
+                      }`}
                     >
-                      <span>{course._id}</span>
-                    </div>
-                    <div
-                      style={{ width: "20px", marginLeft: "7%" }}
-                      className=" flex content-center items-center self-center justify-center p-auto"
-                    >
-                      <span className="font-bold">{course.name}</span>
-                    </div>
-                    <div
-                      style={{
-                        marginLeft: "auto",
-                      }}
-                    >
-                      <span
-                        style={{ width: "20px" }}
-                        className="font-bold  ml-auto"
+                      <div
+                        style={{
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          width: "240px",
+                        }}
                       >
-                        {course.credits}
-                      </span>
-                    </div>
-                    <div className="ml-28">
-                      <span className="font-bold">{course.level}</span>
-                    </div>
-                    <div className="align-center text-end ml-auto mr-10 ">
-                      <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                          More
-                        </Dropdown.Toggle>
+                        <span>{course._id}</span>
+                      </div>
+                      <div
+                        style={{ width: "20px", marginLeft: "7%" }}
+                        className=" flex content-center items-center self-center justify-center p-auto"
+                      >
+                        <span className="font-bold">{course.name}</span>
+                      </div>
+                      <div
+                        style={{
+                          marginLeft: "auto",
+                        }}
+                      >
+                        <span
+                          style={{ width: "20px" }}
+                          className="font-bold  ml-auto"
+                        >
+                          {course.credits}
+                        </span>
+                      </div>
+                      <div className="ml-28">
+                        <span className="font-bold">{course.level}</span>
+                      </div>
+                      <div className="align-center text-end ml-auto mr-10 ">
+                        <Dropdown>
+                          <Dropdown.Toggle
+                            variant="success"
+                            id="dropdown-basic"
+                          >
+                            More
+                          </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                          <Dropdown.Item
-                            onClick={() => {
-                              setCurrentCourse(course);
-                              setShowUpdateCourseModal(true);
-                            }}
-                          >
-                            Update
-                          </Dropdown.Item>
-                          <Dropdown.Item
-                            onClick={() => {
-                              setCurrentCourse(course);
-                              setShowCourseModal(true);
-                            }}
-                          >
-                            View Course info
-                          </Dropdown.Item>
-                          <Dropdown.Item
-                            onClick={() => {
-                              setCurrentCourse(course);
-                              setShowDeleteObjectModal(true);
-                            }}
-                          >
-                            Delete Course
-                          </Dropdown.Item>
-                        </Dropdown.Menu>
-                      </Dropdown>
+                          <Dropdown.Menu>
+                            <Dropdown.Item
+                              onClick={() => {
+                                setCurrentCourse(course);
+                                setShowUpdateCourseModal(true);
+                              }}
+                            >
+                              Update
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                              onClick={() => {
+                                setCurrentCourse(course);
+                                setShowCourseModal(true);
+                              }}
+                            >
+                              View Course info
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                              onClick={() => {
+                                setCurrentCourse(course);
+                                setShowDeleteObjectModal(true);
+                              }}
+                            >
+                              Delete Course
+                            </Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
             </div>
           </>
         ) : (
